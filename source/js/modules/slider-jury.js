35e21af2f2ee85d20efa981015d274af0f9f5e32
Наталья Моисеева <mns-fw@bk.ru>
2023-11-14 23:38:09 +0300

    refactor: header-jury

--- source/js/modules/slider-jury.js
+++ source/js/modules/slider-jury.js
@@ -1,48 +1,43 @@
-import Swiper from '../vendor/swiper';
-
-const jurySlider = document.querySelector('[data-jury-slider]');
-const buttonNext = document.querySelector('[data-jury-next]');
-const buttonPrev = document.querySelector('[data-jury-prev]');
-
 const initJurySlider = () => {
-  if (jurySlider) {
-    // eslint-disable-next-line no-new
-    new Swiper(jurySlider, {
+  const jurySlider = document.querySelector('[data-jury-slider]');
+  const buttonNext = document.querySelector('[data-jury-next]');
+  const buttonPrev = document.querySelector('[data-jury-prev]');
 
-      navigation: {
-        nextEl: buttonNext,
-        prevEl: buttonPrev,
+  return new window.Swiper(jurySlider, {
+    navigation: {
+      nextEl: buttonNext,
+      prevEl: buttonPrev,
+    },
+
+    loop: true,
+    autoHeight: false,
+    breakpoints: {
+      1366: {
+        slidesPerView: 4,
+        spaceBetween: 40,
+        allowTouchMove: false,
       },
 
-      loop: true,
-      breakpoints: {
-        1366: {
-          slidesPerView: 4,
-          spaceBetween: 40,
-          allowTouchMove: false,
-        },
-
-        1200: {
-          slidesPerView: 3,
-          spaceBetween: 40,
-          allowTouchMove: false,
-        },
-
-        768: {
-          slidesPerView: 2,
-          spaceBetween: 32,
-          initialSlide: 2,
-          allowTouchMove: true,
-        },
-
-        320: {
-          slidesPerView: 1,
-          spaceBetween: 0,
-          initialSlide: 2,
-        },
+      1200: {
+        slidesPerView: 3,
+        spaceBetween: 40,
+        allowTouchMove: false,
       },
-    });
-  }
+
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 32,
+        initialSlide: 2,
+        allowTouchMove: true,
+      },
+
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 0,
+        initialSlide: 2,
+      },
+    },
+  });
 };
 
 export {initJurySlider};
