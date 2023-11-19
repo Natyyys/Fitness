6cb076223b03bd4e6ceb7669dc4a0070896fe55a
Наталья Моисеева <mns-fw@bk.ru>
2023-11-19 16:05:27 +0300

    fix(main.js): убирает модальные окна

--- source/js/main.js
+++ source/js/main.js
@@ -1,5 +1,4 @@
 import {iosVhFix} from './utils/ios-vh-fix';
-import {initModals} from './modules/modals/init-modals';
 import {Form} from './modules/form-validate/form';
 import {initJurySlider} from './modules/slider-jury';
 import {initReviewsSlider} from './modules/slider-reviews';
@@ -26,7 +25,6 @@ window.addEventListener('DOMContentLoaded', () => {
   // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
   // в load следует добавить скрипты, не участвующие в работе первого экрана
   window.addEventListener('load', () => {
-    initModals();
     const form = new Form();
     window.form = form;
     form.init();
